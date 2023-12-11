367a0a0d573900fc994fc2eea4bec9c30773fbf8
Sean Brown <seanbrown82@hotmail.com>
2023-12-11 22:13:15 +0000

    feat: add recruiter login page

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,21 @@ import Navigation from './components/Navigation'
 import Home from './pages/Home'
 import Footer from './components/Footer'
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import RecruiterLogIn from "./pages/RecruiterLogIn"
 
 function App() {
 
   return (
     <>
-      <Navigation />
+      
       <Router>
-        <Routes>
-          <Route path="/job-board" element={<Home/>} />
-        </Routes>
+        <Navigation />
+          <Routes>
+            <Route path="/job-board" element={<Home/>} />
+            <Route path="/job-board/recruiter-login" element={ <RecruiterLogIn />} />
+          </Routes>
+        <Footer />
       </Router>
-      <Footer />
     </>
   )
 }
